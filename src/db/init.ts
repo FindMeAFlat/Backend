1306b28927ae246e1a654c35e940228a025b27bf
przemek <host9@o2.pl>
2018-10-02 16:47:05 +0200

    Add endpoint for getting cities

--- src/db/init.ts
+++ src/db/init.ts
@@ -4,6 +4,10 @@ import { Repositories } from '.';
 export function initCities() {
   cities.forEach(city => {
     const existingCity = Repositories.City.single({ name: city });
-    if (!existingCity) Repositories.City.create({ name: city });
+    existingCity.then((founded) => {
+      if(!founded) {
+          Repositories.City.create({ name: city });
+      }
+    });
   });
 }
