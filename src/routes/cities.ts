65cb978c281f3ed552296a75657587a56ebc9523
franczak <host9@o2.pl>
2018-10-06 18:19:33 +0200

    Change let to const

--- src/routes/cities.ts
+++ src/routes/cities.ts
@@ -10,9 +10,8 @@ class CityRouter {
     }
 
     public GetCities(req: Request, res: Response): void {
-        let data;
         Repositories.City.where({}).then((result) => {
-            data = result.map((city) => city.name);
+            const data = result.map((city) => city.name);
             const status = res.statusCode;
             res.json({
                 status,
